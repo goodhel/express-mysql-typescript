852583fb9277c1ad4b81c5386098d82558ab1d24
yudhie123 <yudhie123@gmail.com>
2022-06-16 20:13:40 +0700

    feat: change controller

--- src/controllers/TestController.ts
+++ src/controllers/TestController.ts
@@ -1,16 +1,11 @@
 import { NextFunction, Request, Response, Router } from 'express'
+import response from '../helper/response'
 import m$test from '../modules/test.module'
 
 export const TestController: Router = Router()
 
-TestController.get('/', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const results = await m$test.getList()
-    res.status(200).send(results)
-  } catch (error) {
-    res.status(400).send({
-      status: false,
-      error
-    })
-  }
+TestController.get('/', async (req: Request, res: Response, _next: NextFunction) => {
+  const results = await m$test.getList({ id: +req.params.id })
+
+  response.sendResponse(res, results)
 })
